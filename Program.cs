b46e2df0266c132fa52ba8917312d87759b55c1a
Raihan Mahmud (RAM) <mahmud.raihan@yahoo.com>
2021-09-20 00:33:57 +0600

    project-init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mobile_bill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Start time: ");
+            var startTime = Console.ReadLine();
+            Console.Write("Enter End time: ");
+            var endTime = Console.ReadLine();
+
+        }
+    }
+}
